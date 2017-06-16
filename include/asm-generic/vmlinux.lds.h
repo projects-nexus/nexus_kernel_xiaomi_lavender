9108deb31c0509f766573a65fa890ce22341660e
Sami Tolvanen <samitolvanen@google.com>
2017-06-16 12:52:57 -0700

    FROMLIST: BACKPORT: kbuild: fix dynamic ftrace with clang LTO
    With CONFIG_LTO_CLANG enabled, LLVM IR won't be compiled into object
    files until modpost_link. This change postpones calls to recordmcount
    until after this step.
    
    In order to exclude ftrace_process_locs from inspection, we add a new
    code section .text..ftrace, which we tell recordmcount to ignore, and
    a __norecordmcount attribute for moving functions to this section.
    
    Bug: 62093296
    Bug: 67506682
    Change-Id: Iba2c053968206acf533fadab1eb34a743b5088ee
    (am from https://patchwork.kernel.org/patch/10060327/)
    Signed-off-by: Sami Tolvanen <samitolvanen@google.com>
    Signed-off-by: ImSpiDy <spidy2713@gmail.com>
    Signed-off-by: ImPrashantt <prashant33968@gmail.com>

--- include/asm-generic/vmlinux.lds.h
+++ include/asm-generic/vmlinux.lds.h
@@ -455,6 +455,7 @@
 #define TEXT_TEXT							\
 		ALIGN_FUNCTION();					\
 		*(.text.hot TEXT_MAIN .text.fixup .text.unlikely)	\
+		*(.text..ftrace)					\
 		*(.ref.text)						\
 		*(.text.asan.* .text.tsan.*)				\
 	MEM_KEEP(init.text)						\
