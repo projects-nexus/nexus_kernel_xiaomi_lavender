5a1bf54222220d3ed6709af7e370944f66bbaa14
idkwhoiam322 <idkwhoiam322@raphielgang.org>
2019-10-07 06:17:24 +0530

    schedutil: disable iowait boost
    Signed-off-by: idkwhoiam322 <idkwhoiam322@raphielgang.org>
    Signed-off-by: nysascape <jago@nysascape.digital>
    Signed-off-by: Dušan Uverić <dusan.uveric9@gmail.com>
    Signed-off-by: sohamxda7 <sensoham135@gmail.com>
    Signed-off-by: Prashant-1695 <prashant33968@gmail.com>
    Signed-off-by: ImPrashantt <prashant33968@gmail.com>

--- kernel/sched/cpufreq_schedutil.c
+++ kernel/sched/cpufreq_schedutil.c
@@ -723,7 +723,7 @@ static int sugov_init(struct cpufreq_policy *policy)
                 }
 	}
 
-	tunables->iowait_boost_enable = policy->iowait_boost_enable;
+		tunables->iowait_boost_enable = false;
 
 	policy->governor_data = sg_policy;
 	sg_policy->tunables = tunables;
