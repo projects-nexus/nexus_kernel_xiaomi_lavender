3030fbeefcf5001c32523ede54f5f816a05fb822
ImPrashantt <prashant33968@gmail.com>
2022-04-30 13:00:02 +0000

    mm:swap: fix compilation after vmalloc backport
    Signed-off-by: ImPrashantt <prashant33968@gmail.com>

--- mm/swap.c
+++ mm/swap.c
@@ -1154,7 +1154,6 @@ EXPORT_SYMBOL(pagevec_lookup_range_nr_tag);
  */
 void __init swap_setup(void)
 {
-	unsigned long megs = totalram_pages >> (20 - PAGE_SHIFT);
 #ifdef CONFIG_SWAP
 	int i;
 
@@ -1162,11 +1161,6 @@ void __init swap_setup(void)
 		spin_lock_init(&swapper_spaces[i].tree_lock);
 #endif
 
-	/* Use a smaller cluster for small-memory machines */
-	if (megs < 16)
-		page_cluster = 2;
-	else
-		page_cluster = 3;
 	/*
 	 * Right now other parts of the system means that we
 	 * _really_ don't want to cluster much more
